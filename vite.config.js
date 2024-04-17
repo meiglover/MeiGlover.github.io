1491ad4cc2c9d7eafcad147bf201fa2f3de5d918
CoupleInGlove <coupleinglove@gmail.com>
2024-04-17 17:09:23 -0500

    Removed base.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/CoupleInGlove.github.io/',
+  base: '',
   plugins: [vue()],
   resolve: {
     alias: {
