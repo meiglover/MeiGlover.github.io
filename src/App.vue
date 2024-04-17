63a9519e15c3794c6834fb0589eb729b8928fd06
CoupleInGlove <coupleinglove@gmail.com>
2024-04-17 17:44:11 -0500

    Added OnlyFans button.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <div>
-    <Button>hi</Button>
-    <Button>bye</Button>
-    <Button>bye2</Button>
+    <RouterLink to="onlyfans.com/coupleinglove">
+      <Button label="OnlyFans" />
+    </RouterLink>
   </div>
 </template>
 
