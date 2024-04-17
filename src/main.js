cdbfce2cfa3d7923fb81b842066bf68421e2f5b1
CoupleInGlove <coupleinglove@gmail.com>
2024-04-17 16:33:25 -0500

    Switched to using Vue and PrimeVue.

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/aura-light-pink/theme.css'
+
+import Button from 'primevue/button'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVue)
+
+app.component('Button', Button)
+
+app.mount('#app')
