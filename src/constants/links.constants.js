04fb3b9fa195f3cb7c2957315b812aa116e8898a
CoupleInGlove <coupleinglove@gmail.com>
2025-01-29 13:18:29 -0600

    Updated feature.

--- src/constants/links.constants.js
+++ src/constants/links.constants.js
@@ -85,5 +85,5 @@ export const SocialLinks = [
     link: 'https://www.pornhub.com/model/coupleinglove'
   }
 ]
-export const FeatureLink = 'https://www.manyvids.com/Video/6262229/prostate-exam-gloved-handjob'
+export const FeatureLink = 'https://www.clips4sale.com/studio/269505/30475783/dental-exam-prostate-exam-joi'
 export const FixedLink = 'https://forms.gle/b1tckfmD1ahsRqYQ9'
