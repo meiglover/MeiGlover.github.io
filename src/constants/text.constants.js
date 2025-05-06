2186db4b8fb566a0abb31b4e9f4bd7b4cb84953b
meiglover <meiglover.da@gmail.com>
2025-05-06 17:06:03 -0500

    Updated text.

--- src/constants/text.constants.js
+++ src/constants/text.constants.js
@@ -1,4 +1,4 @@
 export const SubscriptionNotice =
   'Subscribers will ALWAYS be first priority and get the best prices on all paid content.'
 export const RequestNotice =
-  "We can't often message on socials, but if you would like a custom, fill out our form!"
+  "I can't often message on socials, but if you would like a custom, fill out my form!"
